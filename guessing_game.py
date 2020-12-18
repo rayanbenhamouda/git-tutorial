757ae9553e29a8bdba39f4af8f39387acee87818
rayanbenhamouda <rayan.ben_hamouda@edu.devinci.fr>
2020-12-18 12:13:56 +0100

    jeu python amélioration2 : nombre d'essaye -> 5

--- guessing_game.py
+++ guessing_game.py
@@ -7,26 +7,36 @@ import random
 def guess_loop():
     pseudo = input("enter your username: ")
     # This is the number the user will have to guess, chosen randomly in between 1 and 100
+    nbr_essaie = 5
     number_to_guess = random.randint(1, 100)
     print("I have in mind a number in between 1 and 100, can you find it?")
- 
+    print("you have 5 tries")
     # Replay the question until the user finds the correct number
     while True:
         try:
             # Read the number the user inputs
             guess = int(input())
- 
             # Compare it to the number to guess
             if guess > number_to_guess:
                 print("The number to guess is lower")
+                nbr_essaie = nbr_essaie-1
+                if nbr_essaie == 0:
+                    print("number of tests awaits, you've lost to the game: ",pseudo,", the number was:",number_to_guess)
+                    return
             elif guess < number_to_guess:
                 print("The number to guess is higher")
+                nbr_essaie = nbr_essaie-1 
+                if nbr_essaie == 0:
+                    print("number of tests awaits, you've lost to the game: ",pseudo,", the number was:",number_to_guess)
+                    return
             else:
                 # The user found the number to guess, let's exit
                 print("You just found the number, it was indeed", guess)
                 print("play well", pseudo) 
                 return
         # A ValueError is raised by the int() function if the user inputs something else than a number
+            print("number of trials remaining:", nbr_essaie)
+
         except ValueError as err:
             print("Invalid input, please enter an integer")
  
